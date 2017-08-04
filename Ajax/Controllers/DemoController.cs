b825d9d6156d09266ba95387b2f4282e4ee1a276
ravikandel1 <ravkdl@hotmail.com>
2017-08-04 13:44:44 +0545

    Final

---
+++ Ajax/Controllers/DemoController.cs
@@ -0,0 +1,41 @@
+﻿using Ajax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ajax.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+        DemoContext context = new DemoContext();
+        public ActionResult Index()
+        {
+            return View();
+        } 
+
+        [HttpPost]
+        public ActionResult Create(Demo demo)
+        {
+            //if(ModelState.IsValid)
+            //{
+                context.Demos.Add(demo);
+                context.SaveChanges();
+            //}
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+
+        public JsonResult GetDemos(string id)
+        {
+            List<Demo>demos = new List<Demo>();
+            demos = context.Demos.ToList();
+            return Json(demos, JsonRequestBehavior.AllowGet);
+        }
+
+
+	}
+}
---
+++ Ajax/Models/Demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ajax.Models
+{
+    public class Demo
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string MiddleName { get; set; }
+        [Required]
+        public string SurName { get; set; }
+       
+    }
+}
